0b969fb0e3a54d9901722a416a34b34252656918
Levir Barbosa <barbosalevir3@gmail.com>
2026-09-12 11:57:03 -0400

    Add loops to display numbers and text

---
+++ for.js
@@ -0,0 +1,11 @@
+for(let i = 1; i <=10; i++){
+    document.writeln(i+",")
+}
+document.writeln("<br>")
+for(let i = 100 ; i >=2; i-=2){
+    document.writeln(i+"")
+}
+document.writeln("<br>")
+for(let j = 0; i < 20; j++){
+    document.writeln("Levir <br>")
+}
