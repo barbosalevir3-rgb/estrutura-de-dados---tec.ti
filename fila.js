5b76337f615e1af4109bbdc5a5fecff92c79ae41
Levir Barbosa <barbosalevir3@gmail.com>
2026-09-12 11:56:14 -0400

    Implement number sorting based on parity

---
+++ fila.js
@@ -0,0 +1,30 @@
+//const num = [0]
+//let numero = parseInt(prompt("Digite um numero"))
+  //  if(numero %2 == 0){
+    //numero.unshift
+    //alert("Par")
+    //alert(num)
+    //}else{
+    //numero.push
+    //alert("Impar")
+    //alert(num)
+//}
+//alert(numero)
+const numeros = [0]
+var termos = parseInt(prompt("Qunatos termos"))
+for(let i = 0; i < termos; i++){
+let numero = parseInt(prompt())
+if(numero %2==0)
+    numeros.unshift(numero)
+else
+    numeros.push(numero)
+}alert(numeros)
+
+//var termos = parseInt(prompt("Quantos termos"))
+// for(let i = 0; i < termos; i++){
+//let numero = parseInt(prompt())
+//if(numero %2==0)
+// numeros.unshift
+//else
+// numeros.push
+//alert
