c9bfe59f494425abd61aed37203ff76eab44952b
Levir Barbosa <barbosalevir3@gmail.com>
2026-09-12 11:59:58 -0400

    Implement factorial function in funcaofatorial.js

---
+++ funcaofatorial.js
@@ -0,0 +1,10 @@
+function fatorial(){
+  let fatorial = 1, i = 1
+  while(i <= numero){
+    fatorial*=i
+    i++
+  }
+   return fatorial
+}
+let numero = parseInt(prompt("Número"))
+alert(`${numero}!=${fatorial()}`)
